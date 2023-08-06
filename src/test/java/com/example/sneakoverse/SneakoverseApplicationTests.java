8163892a9a1d243922c089edd7f24177b1b1f4ab
Stha07anshu <105202993+Stha07anshu@users.noreply.github.com>
2023-08-06 14:53:57 +0545

    Web whole project

---
+++ src/test/java/com/example/sneakoverse/SneakoverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sneakoverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakoverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
